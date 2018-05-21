291b814cc34e05c2d2d40b6c3c38abe221bb21b3
chachamaru0802 <chachamaru.0802.k@gmail.com>
2018-05-22 01:52:04 +0900

    印刷サンプル 実装

---
+++ PrintSample/PrintSample/PrintSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using PrintSample.DependencyServices;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintSample.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        readonly IPrintService _printService;
+
+        public DelegateCommand PrintCommand => new DelegateCommand(async () => await Print());
+
+        public MainPageViewModel(INavigationService navigationService, IDeviceService deviceService, IPageDialogService pageDialogService, IDependencyService dependencyService)
+            : base(navigationService, deviceService, pageDialogService, dependencyService)
+        {
+            Title = "Main Page";
+
+            _printService = _dependencyService.Get<IPrintService>();
+        }
+
+        private async Task Print()
+        {
+            try
+            {
+                await _printService.PrintAsync("test.pdf");
+            }
+            catch (Exception e)
+            {
+                await base.ShowErrorMessage(e);
+            }
+
+        }
+    }
+}
---
+++ PrintSample/PrintSample/PrintSample/ViewModels/ViewModelBase.cs
@@ -0,0 +1,84 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintSample.ViewModels
+{
+    public class ViewModelBase : BindableBase, INavigationAware, IDestructible
+    {
+        /// <summary>
+        /// DependencyService
+        /// DI取得用
+        /// </summary>
+        protected readonly IDependencyService _dependencyService;
+
+        /// <summary>
+        /// NavigationService
+        /// </summary>
+        readonly INavigationService _navigationService;
+
+        /// <summary>
+        /// デバイス情報サービス
+        /// </summary>
+        readonly IDeviceService _deviceService;
+
+        /// <summary>
+        /// メッセージダイアログサービス
+        /// </summary>
+        readonly IPageDialogService _pageDialogService;
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public ViewModelBase(INavigationService navigationService, IDeviceService deviceService, IPageDialogService pageDialogService , IDependencyService dependencyService)
+        {
+            _navigationService = navigationService;
+            _deviceService = deviceService;
+            _pageDialogService = pageDialogService;
+            _dependencyService = dependencyService;
+        }
+
+        internal async Task ShowMessage(string title, string message)
+        {
+            await _pageDialogService.DisplayAlertAsync(title, message, "OK");
+        }
+
+        internal async Task ShowErrorMessage(Exception e)
+        {
+            await _pageDialogService.DisplayAlertAsync("Error", e.Message, "OK");
+        }
+
+        #region Navigation関連
+
+        public virtual void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        #endregion
+
+        public virtual void Destroy()
+        {
+
+        }
+    }
+}
